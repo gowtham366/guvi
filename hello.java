b41418bd7e474f4eed250b272e47eab7fabca254
Gowtham C <gowtham366.vpi@gmail.com>
2017-06-02 05:52:07 -0400

    Create new file

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class hello{
+	public static void main (String[] args) {
+		Scanner in=new Scanner(System.in);
+		for(int i=0;i<5;i++)
+		{
+		    System.out.print("Hello");
+	    }
+	}
+}
