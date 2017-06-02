f8e7e9740a96e20a6d800b9a75480ca0368d04cf
Gowtham C <gowtham366.vpi@gmail.com>
2017-06-02 12:24:38 +0530

    Create checkchar.java

---
+++ checkchar.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class checkchar {
+	public static void main (String[] args) {
+		//code
+		Scanner in=new Scanner(System.in);
+		char a=in.next().charAt(0);
+		if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+		    System.out.print("vowel");
+		else	
+		    System.out.print("consonent");
+	}
+}
