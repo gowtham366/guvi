1fac59009bfea4b13ec0736dbba2f7cc30e73cf1
Gowtham C <gowtham366.vpi@gmail.com>
2017-06-05 15:34:26 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.lang.*;
+class Power {
+	public static void main (String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		System.out.print((int)Math.pow(n,n));
+	}
+}
