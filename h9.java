a39579bb84409f219e1152e647cbab1490266961
Gowtham C <gowtham366.vpi@gmail.com>
2017-06-07 15:56:52 +0530

    Create h9.java

---
+++ h9.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class h9{
+    public static void main(String[] args) 
+    {
+        Scanner in=new Scanner(System.in);
+        int size=in.nextInt();
+        if(size>0)
+        {
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++)
+            arr[i]=in.nextInt();
+        //Arrays.sort(arr);
+        for(int i:arr) 
+        {
+            for(int j:arr)
+            {
+                    if((i+j)==0)
+                    System.out.println(i+" "+j);
+            }
+        }
+        }
+        else
+        System.out.print("0");
+    }
+}
