db9aaf2cd7014e6c4d310b703f24ea5403985c23
Gowtham C <gowtham366.vpi@gmail.com>
2017-06-07 15:00:44 +0530

    Create h1.java

---
+++ h1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class h1{
+    public static void main(String[] args) 
+    {
+        Scanner in=new Scanner(System.in);
+        int size=in.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++)
+            arr[i]=in.nextInt();
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i:arr) 
+        {
+            if(set.add(i) == false) 
+                System.out.println(i);
+        }
+    }
+}
