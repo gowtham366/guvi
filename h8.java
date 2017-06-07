d0fd3ae8ce6d8d3c58a7148f73d3801c899dcd82
Gowtham C <gowtham366.vpi@gmail.com>
2017-06-07 15:47:08 +0530

    Create h8.java

---
+++ h8.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class h8{
+    public static void main(String[] args) 
+    {
+        Scanner in=new Scanner(System.in);
+        int size=in.nextInt();
+        if(size>0)
+        {
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++)
+            arr[i]=in.nextInt();
+        Arrays.sort(arr);
+        for(int i:arr) 
+        {
+            for(int j:arr)
+            {
+                for(int k:arr)
+                {
+                    if((i+j)==k)
+                    System.out.println(i+" "+j+" "+k);
+                }
+            }
+        }
+        }
+        else
+        System.out.print("0");
+    }
+}
