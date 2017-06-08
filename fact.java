ec5f08cb747be6cc97bab01f29e11880c658b391
Gowtham C <gowtham366.vpi@gmail.com>
2017-06-08 11:57:27 +0530

    Create fact.java

--- fact.java
+++ fact.java
@@ -1,14 +1,14 @@
 import java.util.*;
-class Factorial{
+class fact{
     public static void main(String gow[])
     {
         Scanner in=new Scanner(System.in);
         int n=in.nextInt();
-        int fact=1;
+        int f=1;
         for(int i=1;i<=n;i++)
             {
-                fact*=i;
+                f*=i;
             }
-            System.out.print(fact);
+            System.out.print(f);
     }
 }
