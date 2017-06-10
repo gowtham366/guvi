17c435fe6fef2962fd4aee1e9857470104d2a61b
Gowtham C <gowtham366.vpi@gmail.com>
2017-06-10 12:30:00 +0530

    Create Evennumbers.java

---
+++ Evennumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Evennumbers{  
+ public static void main(String args[]){  
+    Scanner in=new Scanner(System.in);
+    int fst=in.nextInt();
+    int lst=in.nextInt();
+    for(int i=fst;i<=lst;i++)
+    {
+        if(i%2==0)
+        System.out.println(i);
+    }    
+    }
+}  
